cbd4b10770c74a19ca98eb5a532a8e1f0eef59e9
WilsonLng <wilsonzzz19@gmail.com>
2023-10-20 12:36:50 +0800

    Updated function that processes piece positions

--- motion_pkg/motion_pkg/angles_producer.py
+++ motion_pkg/motion_pkg/angles_producer.py
@@ -14,70 +14,86 @@ class angleNode(Node):
     def callback(self, msg):
         output_msg = DecimalPosition()
 
-        R = 3
-        d = 1.5                         # Distance between robotic arm and chess board
-        side = 2.75
+        positions = msg.pos.split()
+        pos1 = positions[0]
+        pos2 = positions[1]
+        mov = positions[2]
 
-        num = 1
-        hordict = {}
+        def anglesProduce(position: str):
+            R = 3
+            d = 1.5                         # Distance between robotic arm and chess board
+            side = 2.75
 
-        for i in "abcdefgh":
-            hordict[i] = num
-            num +=1
+            num = 1
+            hordict = {}
 
-        def findXf(pos):
-            ver = int(pos[1])
-            hor = hordict[pos[0]]
-            return (R + ((R + d + side*(ver - 1) + (side/2))**2 + (side*(hor - 5) + (side/2))**2)**(1/2))
+            for i in "abcdefgh":
+                hordict[i] = num
+                num +=1
 
-        def findYawRad(pos):
-            ver = int(pos[1])
-            hor = hordict[pos[0]]
-            return (math.atan((side*(hor - 5) + (side/2)) / (R + d + side*(ver - 1) + (side/2))))*(180/math.pi)+150
+            def findXf(pos):
+                ver = int(pos[1])
+                hor = hordict[pos[0]]
+                return (R + ((R + d + side*(ver - 1) + (side/2))**2 + (side*(hor - 5) + (side/2))**2)**(1/2))
 
-        xf = findXf(msg.data)
-        yf = 0
+            def findYawRad(pos):
+                ver = int(pos[1])
+                hor = hordict[pos[0]]
+                return (math.atan((side*(hor - 5) + (side/2)) / (R + d + side*(ver - 1) + (side/2))))*(180/math.pi)+150
 
-        l1 = 15.75
-        l2 = 13.25
-        l3 = 17.40
+            xf = findXf(position)
+            yf = 0
+
+            l1 = 15.75
+            l2 = 13.25
+            l3 = 17.40
+            
+            k = xf ** 2 + yf ** 2 + l1 ** 2 - l2 ** 2 + 2 * yf * l3 + l3 ** 2
+            n = math.sqrt((2 * xf * l1) ** 2 + (2 * yf * l1 + 2 * l1 * l3) ** 2)
+
+            alphr = math.atan((2 * yf * l1 + 2 * l1 * l3) / (2 * xf * l1))
+            alphd = math.degrees(alphr)
+
+            thetas = []
+
+            theta11 = alphd + math.degrees(math.acos(k / n))
+            theta12 = alphd - math.degrees(math.acos(k / n))
+
+            theta21 = - theta11 + math.degrees(math.acos(((xf - l1 * math.cos(math.radians(theta11))) / l2)))
+            theta22 = - theta12 + math.degrees(math.acos(((xf - l1 * math.cos(math.radians(theta12))) / l2)))
+
+            theta31 = 270 - theta11 - theta21
+            theta32 = 270 - theta12 - theta22
+
+            thetas.extend([theta11, theta12, theta21, theta22, theta31, theta32])
+
+            for i in range(len(thetas)):
+                if thetas[i] < 0:
+                    thetas[i] = thetas[i] + 360
+                elif thetas[i] > 360:
+                    thetas[i] = thetas[i] - 360
+
+            for i in range(2, len(thetas)):
+                thetas[i] = thetas[i] - 120
+
+            thetas[0] = thetas[0] + 60
+            thetas[1] = thetas[1] + 60
+            thetas[2] = 300 - thetas[2]
+
+            return [findYawRad(position), thetas[0], thetas[2], thetas[4]]
         
-        k = xf ** 2 + yf ** 2 + l1 ** 2 - l2 ** 2 + 2 * yf * l3 + l3 ** 2
-        n = math.sqrt((2 * xf * l1) ** 2 + (2 * yf * l1 + 2 * l1 * l3) ** 2)
+        goal1Angles = anglesProduce(pos1)
+        goal2Angles = anglesProduce(pos2)
 
-        alphr = math.atan((2 * yf * l1 + 2 * l1 * l3) / (2 * xf * l1))
-        alphd = math.degrees(alphr)
-
-        thetas = []
-
-        theta11 = alphd + math.degrees(math.acos(k / n))
-        theta12 = alphd - math.degrees(math.acos(k / n))
-
-        theta21 = - theta11 + math.degrees(math.acos(((xf - l1 * math.cos(math.radians(theta11))) / l2)))
-        theta22 = - theta12 + math.degrees(math.acos(((xf - l1 * math.cos(math.radians(theta12))) / l2)))
-
-        theta31 = 270 - theta11 - theta21
-        theta32 = 270 - theta12 - theta22
-
-        thetas.extend([theta11, theta12, theta21, theta22, theta31, theta32])
-
-        for i in range(len(thetas)):
-            if thetas[i] < 0:
-                thetas[i] = thetas[i] + 360
-            elif thetas[i] > 360:
-                thetas[i] = thetas[i] - 360
-
-        for i in range(2, len(thetas)):
-            thetas[i] = thetas[i] - 120
-
-        thetas[0] = thetas[0] + 60
-        thetas[1] = thetas[1] + 60
-        thetas[2] = 300 - thetas[2]
-
-        output_msg.angle1 = findYawRad(msg.data)
-        output_msg.angle2 = thetas[0]
-        output_msg.angle3 = thetas[2]
-        output_msg.angle4 = thetas[4]
+        output_msg.angle01 = goal1Angles[0]
+        output_msg.angle02 = goal1Angles[1]
+        output_msg.angle03 = goal1Angles[2]
+        output_msg.angle04 = goal1Angles[3]
+        output_msg.angle11 = goal2Angles[0]
+        output_msg.angle12 = goal2Angles[1]
+        output_msg.angle13 = goal2Angles[2]
+        output_msg.angle14 = goal2Angles[3]
+        output_msg.move = mov
 
         # Populate the output message
         self.publisher.publish(output_msg)
