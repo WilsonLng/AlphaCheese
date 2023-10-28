033f71d252032b10156d93949803ae129cf0df58
stevennedwinn <stevennedwinn@gmail.com>
2023-10-28 22:12:20 +0800

    [UPDATE] testCamModel: added mapping for each boxes

--- comp_vision/comp_vision/testCamModel.py
+++ comp_vision/comp_vision/testCamModel.py
@@ -3,9 +3,108 @@ import cv2 as cv
 import json
 from roboflow import Roboflow
 import matplotlib.pyplot as plt
+from shapely.geometry import LineString
 
 chessboard_path = 'chessboard-ref.jpg'
 chesspiece_path = 'chesspiece-ref.jpg'
+
+class Trapezium:
+    def __init__(self, trapezium_x, y_coord, image_path):
+        self.trapezium_x = trapezium_x
+        self.y_coord = y_coord
+        self.y11 = max(y_coord)
+        self.y0 = min(y_coord)
+        self.x0 = trapezium_x[0] 
+        self.x1 = trapezium_x[1] 
+        self.x2 = trapezium_x[2]
+        self.x3 = trapezium_x[3] 
+        self.intersection_points = []
+        self.upper_coord = []
+        self.lower_coord = []
+        self.image_path = image_path
+
+    def calculate_val_x(self, point1, point2, y):  
+        x1, y1 = point1
+        x2, y2 = point2
+
+        if x1 == x2:
+            return x1
+
+        m = (y2 - y1) / (x2 - x1)
+        b = y1 - m * x1
+        x = (y - b) / m
+
+        return round(x, 3)
+
+    def plot_graph(self):
+        img = cv.imread(self.image_path)
+        
+        upper_segments = np.linspace(self.x3, self.x2, 9)
+        self.upper_coord = [(i, self.y11) for i in upper_segments]
+        lower_segments = np.linspace(self.x0, self.x1, 9)
+        self.lower_coord = [(i, self.y0) for i in lower_segments]
+
+        horz_coord1 = [(min(self.trapezium_x), j) for j in self.y_coord]
+        horz_coord2 = [(max(self.trapezium_x), j) for j in self.y_coord]
+
+        for i in range(9):
+            line1 = LineString([self.upper_coord[i], self.lower_coord[i]])
+            for k in range(9):
+                line2 = LineString([horz_coord1[k], horz_coord2[k]])
+                intersection = line1.intersection(line2)
+                if intersection:
+                    # print("intersection: ", intersection)
+                    if intersection.geom_type == 'Point':
+                        self.intersection_points.append((intersection.x, intersection.y))
+                        im_chessboard = cv.circle(img, (int(intersection.x), int(intersection.y)), 2, (0,0,255), -1)
+                        # plt.plot(intersection.x, intersection.y, 'ro')
+
+        # for i in range(9):
+        #     plt.plot([self.upper_coord[i][0], self.lower_coord[i][0]], [self.upper_coord[i][1], self.lower_coord[i][1]], 'b--')
+
+        # for i in range(9):
+        #     plt.plot([horz_coord1[i][0], horz_coord2[i][0]], [horz_coord1[i][1], horz_coord2[i][1]], 'g--')
+
+        # plt.show()
+        cv.imshow("test", im_chessboard)  
+
+    def find_pos(self, x1, y1):
+        sorted_intersection_points = np.array(sorted(self.intersection_points, key=lambda p: (p[1], p[0]))).reshape(9, 9, 2)
+        pos = np.zeros((8, 8), dtype=object)
+
+        for i in range(8):
+            for j in range(8):
+                pos[7 - i, j] = chr(j + 97) + str(i + 1)
+
+        # print("pos: ", pos)
+
+        for j in range(len(sorted_intersection_points)-1):
+            if y1 > sorted_intersection_points[j, 0, 1] and y1 < sorted_intersection_points[j + 1, 0, 1]:
+                k = j
+
+        if y1 < min(sorted_intersection_points[:, 0, 1]) or y1 > max(sorted_intersection_points[:, 0, 1]):
+            print("Coordinate outside the range of y")
+        
+        elif y1 in sorted_intersection_points[:, 0, 1]:
+            print("Chess piece is placed on the edge of the squares")
+        
+        else:
+            x_pot = []
+            for i in range(len(self.upper_coord)):
+                point1 = self.upper_coord[i]
+                point2 = self.lower_coord[i]
+                x = self.calculate_val_x(point1, point2, y1)
+                x_pot.append(x)
+            
+            for p in range(len(x_pot)-1):
+                if x1 > float(x_pot[p]) and x1 < float(x_pot[p + 1]):
+                    h = p
+            if x1 < min(x_pot) or x1 > max(x_pot):
+                print("Coorinate outside the range of x")
+            elif x1 in x_pot:
+                print("Chess piece is placed on the edge of the squares")
+            else:
+                print('Chess piece position is:', pos[7-k,h])
 
 def findVertices(json_data,range_ymax = 15,range_ymin =5 ,range_xmax = 100, range_xmin=100):
     
@@ -204,6 +303,31 @@ def main():
             # plt.show()
             cv.imshow("chessboard corners", im_chessboard_corners)
             print ("Chessboard corners plotted")
+
+            # Each boxes
+            trapezium_x = [corners[i][0] for i in [3, 2, 1, 0, 3]]
+            # trapezium_y = [corner[1] for corner in corners]
+            # y_coord = np.linspace((corners[2][1]+corners[3][1])/2, (corners[0][1]+corners[1][1])/2, 9)
+            min_y = (corners[2][1] + corners[3][1]) / 2
+            max_y = (corners[0][1] + corners[1][1]) / 2
+            k = [0.5/6, 0.5/6, 0.65/6, 0.7/6, 0.7/6, 0.85/6, 1/6, 1.1/6]
+
+            y_coord = [max_y]
+            for i in range(7, -1, -1 ):  # 8 more y-values to make a total of 9
+                y_coord.append(y_coord[-1] - (max_y - min_y) *k[i])
+            
+            trapezium = Trapezium(trapezium_x, y_coord, chessboard_path)
+            trapezium.plot_graph()
+            
+
+        # Map Chess Piece
+        if chessboard_mapped:
+            cv.imwrite(chesspiece_path, frame)
+            json_data2 = model2.predict(chesspiece_path).json()
+            # json_str2 = json.dumps(json_data2, indent=4) # pretty json_data2
+            # print (json_str2)
+
+            # segment_res = np.array(json_data['predictions'][0]['points'])
 
         
 
